44c082b53019f49e8dd505a6cd25484cec651e2f
FabricioRomero <fabriciojoseromero@gmail.com>
2026-05-12 20:16:20 -0300

    Se agregaron el movimiento en primera persona y el modelo de la escuela

--- Escenas/jugador.gd
+++ Escenas/jugador.gd
@@ -6,8 +6,23 @@ extends CharacterBody3D
 # La gravedad que afecta al personaje
 @export var gravedad = 75
 
+@export var sensibilidad = 0.002
+
+@onready var camera = $CameraPivot/Camera3D
+
+var topeCam = 0.0
+
 # La velocidad a la que se va a mover el personaje
 var velocidad_objetivo = Vector3.ZERO
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * sensibilidad)
+		topeCam = clamp(topeCam - event.relative.y * sensibilidad, deg_to_rad(-89), deg_to_rad(90))
+		camera.rotation.x = topeCam
 
 func _physics_process(delta: float) -> void:
 	# La direccion en la que se mueve el personaje
@@ -16,31 +31,23 @@ func _physics_process(delta: float) -> void:
 	# Si el jugador presiona la accion asignada a move_right
 	if Input.is_action_pressed("move_right"):
 		# La direccion en x se resta en 1
-		direccion.x -= 1
+		direccion -= transform.basis.x
 	# Si el jugador presiona la accion asignada a move_left
 	if Input.is_action_pressed("move_left"):
 		# La direccion en x se suma en 1
-		direccion.x += 1
+		direccion += transform.basis.x
 	# Si el jugador presiona la accion asignada a move_back
 	if Input.is_action_pressed("move_back"):
 		# La direccion en z se resta en 1
-		direccion.z -= 1
+		direccion -= transform.basis.z
 	# Si el jugador presiona la accion asignada a move_forward
 	if Input.is_action_pressed("move_forward"):
 		# La direccion en z se suma en 1
-		direccion.z += 1
+		direccion += transform.basis.z
 	# Si el jugador presiona la accion asignada a jump
 	if Input.is_action_pressed("jump"):
 		# La direccion en y se suma en 1
-		direccion.y += 1
-	
-	
-	## Si la direccion es distinta a un vector 3 en 0 (osea que se mueve)
-	#if direccion != Vector3.ZERO:
-		## Normaliza la direcion para que no se sumen las velocidades
-		#direccion = direccion.normalized()
-		## Al nodo Pivot lo rota para que mire hacia la direccion que se esta moviendo
-		#$Pivot.basis = Basis.looking_at(direccion)
+		direccion += transform.basis.y
 	
 	# La velocidad objetivo en el eje x es igual a la direccion en el eje x por la velocidad
 	velocidad_objetivo.x = direccion.x * velocidad
@@ -50,8 +57,7 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		# La velocidad objetivo en el eje y es igual a la direccion en el eje y por la velocidad
 		velocidad_objetivo.y = direccion.y * velocidad
-	
-	
+
 	# si no esta en el suelo
 	if not is_on_floor():
 		# La velocidad objetivo en el eje y es igual a la velocidad objetivo en el eje y menos la gravedad por delta
